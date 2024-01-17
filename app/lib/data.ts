09554081ad4347a19a8b9eed742ca0c0294ae1d9
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-01-17 12:55:19 +0700

    [CH7] Fetch Card Data

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -5,6 +5,7 @@ import {
   InvoiceForm,
   InvoicesTable,
   LatestInvoiceRaw,
+  LatestInvoice,
   User,
   Revenue,
 } from './definitions';
@@ -42,7 +43,7 @@ export async function fetchLatestInvoices() {
       },
       take: 5,
     });
-    const latestInvoices = data.map((invoice) => ({
+    const latestInvoices = data.map((invoice: any) => ({
       id: invoice.id,
       name: invoice.customer.name,
       email: invoice.customer.email,
@@ -58,26 +59,19 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
-
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
+    const [numberOfInvoices, numberOfCustomers, invoicesByStatus] = await Promise.all([
+      db.invoices.count(),
+      db.customers.count(),
+      db.invoices.groupBy({
+        by: ['status'],
+        _sum: {
+          amount: true,
+        },
+      }),
     ]);
 
-    const numberOfInvoices = Number(data[0].rows[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].rows[0].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2].rows[0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2].rows[0].pending ?? '0');
+    const totalPendingInvoices = formatCurrency(invoicesByStatus[0]._sum.amount ?? 0);
+    const totalPaidInvoices = formatCurrency(invoicesByStatus[1]._sum.amount ?? 0);
 
     return {
       numberOfCustomers,
