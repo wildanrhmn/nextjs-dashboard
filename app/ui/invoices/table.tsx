c952901125560dfc223395546aebb8a2f43acef1
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-01-17 15:15:36 +0700

    [Bug] Fix Prod Build

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -18,7 +18,7 @@ export default async function InvoicesTable({
       <div className="inline-block min-w-full align-middle">
         <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
           <div className="md:hidden">
-            {invoices?.map((invoice) => (
+            {invoices?.map((invoice: any) => (
               <div
                 key={invoice.id}
                 className="mb-2 w-full rounded-md bg-white p-4"
