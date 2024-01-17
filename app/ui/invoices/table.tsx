61b1b6315987788740a3cc49374a016b955d3f5b
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-01-17 15:19:58 +0700

    [Bug] Fix Prod Build

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -78,7 +78,7 @@ export default async function InvoicesTable({
               </tr>
             </thead>
             <tbody className="bg-white">
-              {invoices?.map((invoice) => (
+              {invoices?.map((invoice: any) => (
                 <tr
                   key={invoice.id}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
