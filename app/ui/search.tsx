1bc5a33a27ade86efc0b44db9b6534528ba1ef16
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-01-20 00:27:34 +0700

    [Feat] Add Debouncing on Search

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,12 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, useRouter, usePathname } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  function handleSearch(term: string) {
+
+  // Debounce search term
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
     if(term){
       params.set('query', term);
@@ -15,7 +18,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
