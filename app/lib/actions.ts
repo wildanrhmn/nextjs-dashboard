6dadc16c1c2f6bf378304677545cf45f1ff55f2f
wildanrhmn <80866806+wildanrhmn@users.noreply.github.com>
2024-01-20 15:00:16 +0700

    [Feat] Add Update and Delete Invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -37,3 +37,38 @@ export async function createInvoice(formData: FormData) {
       revalidatePath('/dashboard/invoices');
       redirect('/dashboard/invoices');
 }
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData){
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+      });
+
+      const amountInCents = amount * 100;
+
+      await db.invoices.update({
+        where: {
+          id: id
+        },
+        data: {
+          customer_id: customerId,
+          amount: amountInCents,
+          status: status
+        }
+      })
+
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string){
+    await db.invoices.delete({
+      where: {
+        id: id
+      }
+    })
+    revalidatePath('/dashboard/invoices');
+}
